076440ead771750ec2003d62005395497d220d7f
hussamkhatib <hussamkhatib20@gmail.com>
2021-07-10 16:12:38 +0530

    feat: add store page

---
+++ components/common/ProductsCard.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { urlFor } from "../../utils/sanity";
+
+function ProductCard({ _id, title, mainImage, slug, defaultProductVariant }) {
+  return (
+    <Link href={`/products/${slug.current}`}>
+      <a className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+        <div
+          className="flex items-end justify-end h-56 w-full bg-cover"
+          style={{
+            backgroundImage: `url('${urlFor(mainImage)
+              .auto("format")
+              .fit("crop")
+              .width(750)
+              .quality(80)}`,
+          }}
+        >
+          <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+cart
+            </button>
+        </div>
+        <div className="px-5 py-3">
+          <h3 className="text-gray-700 uppercase">{title}</h3>
+          <span className="text-gray-500 mt-2">
+            Rs {defaultProductVariant?.price}
+          </span>
+        </div>
+      </a>
+    </Link>
+  );
+}
+
+export default ProductCard;
---
+++ pages/store/index.js
@@ -0,0 +1,32 @@
+import Error from "next/error";
+import { useRouter } from "next/router";
+import { getClient, usePreviewSubscription } from "../../utils/sanity";
+import ProductsPage from "../../components/common/ProductsPage";
+
+const query = `//groq
+  *[_type == "product" && defined(slug.current)]
+`;
+
+function ProductsPageContainer({ productsData, preview }) {
+  const router = useRouter();
+  if (!router.isFallback && !productsData) {
+    return <Error statusCode={404} />;
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data: products } = usePreviewSubscription(query, {
+    initialData: productsData,
+    enabled: preview || router.query.preview !== null,
+  });
+
+  return <ProductsPage products={products} />;
+}
+
+export async function getStaticProps({ params = {}, preview = false }) {
+  const productsData = await getClient(preview).fetch(query);
+
+  return {
+    props: { preview, productsData },
+  };
+}
+
+export default ProductsPageContainer;
