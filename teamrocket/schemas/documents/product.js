15b1edae83f72b48d0599cbc5bcc313b6b2e8cea
Mohammed Saqlain <saqlain14.ms@gmail.com>
2021-07-10 15:20:17 +0530

    add sanity & sample schemas

---
+++ teamrocket/schemas/documents/product.js
@@ -0,0 +1,24 @@
+export default {
+    name: "product",
+    title: "Product",
+    type: "document",
+
+    fields: [
+        {
+            name: "title",
+      title: "Title",
+      type: "string",
+    },
+
+    {
+        title: "Main image",
+        name: "mainImage",
+        type: "image",
+        options: {
+          hotspot: true,
+        },
+      },
+    ]
+
+    
+}
