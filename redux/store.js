4d8198fc4a57d684cdf43af83cd3afba6d8d867d
hussamkhatib <hussamkhatib20@gmail.com>
2021-07-10 08:21:13 +0530

    feat: add slider container
    add reducers to toggle slide in and out,toggle back reducer not implemented yet.

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import slidingContainerReducer from "./sliderSlice";
 
 export default configureStore({
     reducer: {
+      openSlidingComponent: slidingContainerReducer,
     },
   });
   
